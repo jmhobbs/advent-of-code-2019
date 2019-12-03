a653fa2e24b2665854248043e9173da9a0eb0e87
John Hobbs <john@velvetcache.org>
2019-12-02 23:09:31 -0600

    Problem 01:B - Text and tests.

--- 01/src/lib.rs
+++ 01/src/lib.rs
@@ -2,6 +2,10 @@ pub fn find_fuel_requirement(mass: i32) -> i32 {
     mass / 3 - 2
 }
 
+pub fn find_fuel_fuel_requirement(mass: i32) -> i32 {
+    0
+}
+
 #[cfg(test)]
 mod tests {
     // Note this useful idiom: importing names from outer (for mod tests) scope.
@@ -13,5 +17,12 @@ mod tests {
         assert_eq!(find_fuel_requirement(14), 2);
         assert_eq!(find_fuel_requirement(1969), 654);
         assert_eq!(find_fuel_requirement(100756), 33583);
+    }
+
+    #[test]
+    fn test_find_fuel_fuel_requirement() {
+        assert_eq!(find_fuel_fuel_requirement(2), 2);
+        assert_eq!(find_fuel_fuel_requirement(654), 966);
+        assert_eq!(find_fuel_fuel_requirement(33583), 50346);
     }
 }
